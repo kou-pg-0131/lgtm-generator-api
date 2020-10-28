3030f74dcdb6d74077f414eb2a5a41efb3c27df2
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-28 20:04:06 +0900

    自動で文字色を判定するようにした

--- src/infrastructures/lgtmWriter.ts
+++ src/infrastructures/lgtmWriter.ts
@@ -20,10 +20,12 @@ export class LgtmWriter implements ILgtmWriter {
     const context = canvas.getContext('2d');
     context.drawImage(image, 0, 0, image.width, image.height, 0, 0, distWidth, distHeight);
 
+
     // write text
     const x = canvas.width / 2;
     const y = canvas.height / 2;
-    context.fillStyle = '#000000';
+    const textColorHex = this.getTextColorHex(context.getImageData(0, 0, canvas.width, canvas.height).data);
+    context.fillStyle = `#${textColorHex}`;
     context.textAlign = 'center';
     context.textBaseline = 'middle';
     const headerFontSize = canvas.width / 7;
@@ -34,5 +36,29 @@ export class LgtmWriter implements ILgtmWriter {
     context.fillText('L o o k s   G o o d   T o   M e', x, y + headerFontSize / 1.5);
 
     return canvas.toBuffer('image/png');
+  }
+
+  private getColor(data: Uint8ClampedArray): { r: number; g: number; b: number; } {
+    const color = { r: 0, g: 0, b: 0 };
+
+    for (let i = 0; i < data.length; i += 4) {
+      color.r += data[i];
+      color.g += data[i + 1];
+      color.b += data[i + 2];
+    }
+
+    color.r = Math.floor(color.r / (data.length / 4));
+    color.g = Math.floor(color.g / (data.length / 4));
+    color.b = Math.floor(color.b / (data.length / 4));
+
+    return color;
+  }
+
+  private getTextColorHex(data: Uint8ClampedArray): string {
+    const color = this.getColor(data);
+
+    // See: https://www.w3.org/TR/AERT/#color-contrast
+    const brightness = ((color.r * 299) + (color.g * 587) + (color.b * 114)) / 1000;
+    return brightness < 150 ? 'ffffff' : '000000' ;
   }
 }
