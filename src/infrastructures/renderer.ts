31652d33fff44edffb444d6325d231625a15fa1f
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-30 23:00:07 +0900

    renderer をリファクタ

--- src/infrastructures/renderer.ts
+++ src/infrastructures/renderer.ts
@@ -2,27 +2,15 @@ import { IRenderer, IResponse, IHeaders } from '../interfaces/controllers';
 
 export class Renderer implements IRenderer {
   public ok(params: { body: string; contentType: string; }): IResponse {
-    return {
-      statusCode: 200,
-      body: params.body,
-      headers: this.buildHeaders(params.contentType),
-    };
+    return { statusCode: 200, body: params.body, headers: this.buildHeaders(params.contentType) };
   }
 
   public created(params: { body: string; contentType: string; }): IResponse {
-    return {
-      statusCode: 201,
-      body: params.body,
-      headers: this.buildHeaders(params.contentType),
-    };
+    return { statusCode: 201, body: params.body, headers: this.buildHeaders(params.contentType) };
   }
 
   public badRequest(): IResponse {
-    return {
-      statusCode: 400,
-      body: this.buildMessageBody('Bad Request'),
-      headers: this.buildHeaders(),
-    };
+    return { statusCode: 400, body: this.buildMessageBody('Bad Request'), headers: this.buildHeaders() };
   }
 
   private buildMessageBody(message: string): string {
@@ -32,7 +20,7 @@ export class Renderer implements IRenderer {
   private buildHeaders(contentType?: string): IHeaders {
     return {
       'Content-Type': contentType || 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': '*', // TODO: lgtm-generator のものにする
     };
   }
 }
