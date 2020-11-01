5ac869281e3fe6c4a0ab8b1509478ab8754df6f0
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-01 15:36:35 +0900

    prod 環境のインフラを構築

---
+++ terraform/envs/prod/default.tf
@@ -0,0 +1,16 @@
+provider aws {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+module main {
+  source = "../.."
+
+  stage        = var.stage
+  ip_whitelist = var.ip_whitelist
+}
+
+variable ip_whitelist {
+  type    = list(string)
+  default = []
+}
