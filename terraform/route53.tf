6439dcfa2c8b931919136adf875d48268d21ee06
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-02 23:31:00 +0900

    apigateway はカスタムドメイン名で独自ドメイン対応にした

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -22,8 +22,8 @@ resource aws_route53_record api {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.api.domain_name
-    zone_id                = aws_cloudfront_distribution.api.hosted_zone_id
+    name                   = aws_api_gateway_domain_name.main.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.main.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
