e0d07dc09f6c703d964ed57fef0bbf27197af556
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-07 22:20:10 +0900

    ローカルでは LGTM 画像はローカルに保存するようにした

--- src/infrastructures/s3FileStorage.ts
+++ src/infrastructures/s3FileStorage.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import { S3 } from 'aws-sdk';
 import { IFileStorage } from '../interfaces/gateways';
 
@@ -5,12 +6,16 @@ export class S3FileStorage implements IFileStorage {
   constructor(private config: { bucket: string; s3Client: S3; }) {}
 
   public async save(params: { path: string; data: Buffer; contentType: string; }): Promise<void> {
-    await this.config.s3Client.putObject({
-      Bucket: this.config.bucket,
-      Key: params.path,
-      Body: params.data,
-      ContentType: params.contentType,
-    }).promise();
+    if (process.env.IS_LOCAL === 'true' || process.env.IS_OFFLINE === 'true') {
+      fs.writeFileSync(`tmp/lgtms/${params.path}`, params.data);
+    } else {
+      await this.config.s3Client.putObject({
+        Bucket: this.config.bucket,
+        Key: params.path,
+        Body: params.data,
+        ContentType: params.contentType,
+      }).promise();
+    }
   }
 
   public async delete(params: { path: string; }): Promise<void> {
