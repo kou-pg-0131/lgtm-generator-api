0163fdcb02972fb4eecae1a5746f2fe7f90b42cd
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-08 22:14:29 +0900

    HttpClient.get() で responsetype を受け取れるようにした

--- src/infrastructures/httpClient.ts
+++ src/infrastructures/httpClient.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+type ResponseType = 'json' | 'arraybuffer';
+
 export interface IHttpClient {
-  get<T>(url: string): Promise<T>;
+  get<T>(url: string, responseType?: ResponseType): Promise<T>;
 }
 
 export class HttpClient implements IHttpClient {
-  public async get<T>(url: string): Promise<T> {
-    return (await axios.get(url)).data;
+  public async get<T>(url: string, responseType: ResponseType = 'json'): Promise<T> {
+    return (await axios.get(url, { responseType })).data;
   }
 }
