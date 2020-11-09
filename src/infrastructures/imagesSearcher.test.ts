881f4bf810006659f28edbcc0800562c92bc3a0c
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-09 22:40:44 +0900

    テストを修正

--- src/infrastructures/imagesSearcher.test.ts
+++ src/infrastructures/imagesSearcher.test.ts
@@ -5,7 +5,7 @@ describe('ImagesSearcher', () => {
   describe('search()', () => {
     it('should call httpClient.get() with correct args', async () => {
       const urlBuilder: IUrlBuilder = { build: jest.fn().mockReturnValue('https://example.com'), join: jest.fn() };
-      const httpClient: IHttpClient = { get: jest.fn().mockResolvedValue({ items:[{ title: 'TITLE', link: 'LINK' }] }) };
+      const httpClient: IHttpClient = { get: jest.fn().mockResolvedValue({ items:[{ title: 'TITLE', link: 'https://LINK' }] }) };
       const imagesSearcher = new ImagesSearcher({
         apiKey: 'API_KEY',
         searchEngineId: 'SEARCH_ENGINE_ID',
@@ -14,7 +14,7 @@ describe('ImagesSearcher', () => {
       });
 
       const images = await imagesSearcher.search({ q: 'QUERY' });
-      expect(images).toEqual([{ title: 'TITLE', url: 'LINK' }]);
+      expect(images).toEqual([{ title: 'TITLE', url: 'https://LINK' }]);
       expect(urlBuilder.build).toHaveBeenCalledWith('https://customsearch.googleapis.com/customsearch/v1', {
         key: 'API_KEY', q: 'QUERY', cx: 'SEARCH_ENGINE_ID', num: 10, searchType: 'image', safe: 'active', fileType: 'jpeg',
       });
